c2b11a85df4667661639335a8de78afe5d257a7f
Eric H <eek1601@gmail.com>
2022-06-15 17:04:43 -0400

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import requests
+import json
+from datetime import datetime
+import config
+
+root = Tk()
+root.geometry('400x400')
+root.resizable(0, 0)
+root.title("Weather App")
+
